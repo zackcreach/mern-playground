f13fc79895f89f3560a38994d85e06bcaee73418
Zack Creach <zackcreach@gmail.com>
2018-06-02 14:14:28 -0400

    Tests with app locals and templating.

--- app.js
+++ app.js
@@ -9,16 +9,25 @@ const listening = () => {
     proxy: `localhost:${port}`,
     port: port + 1,
     files: ["app.js", "views/**/*", "src/**/*"],
-    open: false,
     ui: false,
+    open: false,
+    online: false,
   });
 };
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.locals.react = "global react local property";
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    navigation: ["home&amp;", "&amp;about", "portfolio&amp;"],
+  });
 });
 
 app.listen(port, listening);
